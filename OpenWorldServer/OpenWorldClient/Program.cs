c30efe5902619e7a87395b0d284a612dee9f1c40
Toofan Tahmouresi <toofan.tah@gmail.com>
2022-12-28 23:30:59 +0300

    MMO Client

---
+++ OpenWorldServer/OpenWorldClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Text;
+
+public class OpenWordClient : TcpClient
+{
+    public static void Main(string[] args)
+    {
+        var server = new IPEndPoint(IPAddress.Loopback, 1313);
+        var client = new IPEndPoint(IPAddress.Loopback, 1314);
+
+        while (true)
+        {
+            var udpClient = new UdpClient(client);
+            Console.WriteLine("Please enter a string, between 1-20 characters");
+            string inputTMP = Console.ReadLine();
+            if (inputTMP != null)
+            {
+                var msg = Encoding.ASCII.GetBytes(inputTMP);
+                udpClient.Send(msg, msg.Length, server);
+            }
+            udpClient.Close();
+        }
+    }
+}
